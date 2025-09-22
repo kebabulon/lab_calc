e0349fee84d6d1e49a6eddfbb7ed3aff5bc1eb50
kebabulon <3dmod07@mail.ru>
2025-09-23 01:04:10 +0300

    Implement rpn solver

--- src/rpn.py
+++ src/rpn.py
@@ -1,7 +1,8 @@
-from src.tokenize import Token
+from src.calculation_exceptions import ExpresionException
+from src.calculation_exceptions import CalculationException
+from src.token import Token, TokenName
 
 
-# TODO: implement
 def solve_rpn(rpn_expr: list[Token]) -> float:
     """
     Вычисляет математическое выражение в обратной польской нотации (RPN)
@@ -9,4 +10,73 @@ def solve_rpn(rpn_expr: list[Token]) -> float:
     :return: Возвращает результат выполнения вырожения
     """
 
-    return 0.0
+    stack: list[int | float] = []
+
+    for token in rpn_expr:
+        if token.is_number():
+            assert token.value is not None
+            stack.append(token.value)
+
+        elif token.is_operator():
+            if token.get_arg_count() == 1:
+                n = stack.pop()  # number
+
+                match token.name:
+                    case TokenName.UNARY_PLUS:
+                        result = +n
+
+                    case TokenName.UNARY_MINUS:
+                        result = -n
+
+                    case _:
+                        raise ExpresionException(f"Неизвестная операция ('{token}')")
+
+            elif token.get_arg_count() == 2:
+                n2 = stack.pop()  # number 2
+                n1 = stack.pop()  # number 1
+
+                match token.name:
+                    case TokenName.ADD:
+                        result = n1 + n2
+
+                    case TokenName.SUBTRACT:
+                        result = n1 - n2
+
+                    case TokenName.MULTIPLY:
+                        result = n1 * n2
+
+                    case TokenName.DIVIDE:
+                        if n2 == 0:
+                            raise CalculationException(f"Нельзя делить на ноль ({n1} / {n2})")
+                        result = n1 / n2
+
+                    case TokenName.DIVIDE_AND_FLOOR:
+                        if n2 == 0:
+                            raise CalculationException(f"Нельзя делить на ноль ({n1} // {n2})")
+                        if (type(n1) is not int) or (type(n2) is not int):
+                            raise CalculationException(f"Делить с окргулением можно только целые числа ({n1} // {n2})")
+                        result = n1 // n2
+
+                    case TokenName.MOD:
+                        if n2 == 0:
+                            raise CalculationException(f"Нельзя брать остаток числа от нуля ({n1} % {n2})")
+                        if (type(n1) is not int) or (type(n2) is not int):
+                            raise CalculationException(f"Брать остаток можно только с целыми числами ({n1} % {n2})")
+                        result = n1 % n2
+
+                    case TokenName.POW:
+                        result = n1 ** n2
+
+                    case _:
+                        raise ExpresionException(f"Неизвестная операция ('{token}')")
+
+            else:
+                raise ExpresionException(f"Неизвестная операция ('{token}')")
+
+            stack.append(result)
+
+    final_result = stack.pop()
+    if stack:
+        raise ExpresionException("Ошибка синтаксиса математического вырожения")
+
+    return final_result
