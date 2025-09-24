a36c6d272cb278ea15c36bb089d86292a417a4ca
kebabulon <3dmod07@mail.ru>
2025-09-24 22:24:23 +0300

    Add `test_expresion_syntax` test

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -5,6 +5,10 @@ from src.calculate import calculate
 
 
 def test_number_tokenization():
+    """
+    Проверяет токенизирования числа
+    :return: Данная функция ничего не возвращает
+    """
     calculate("0")
     calculate("1234567890")
     calculate("1234.56789")
@@ -19,3 +23,53 @@ def test_number_tokenization():
         calculate(".")
     with pytest.raises(ExpresionException):
         calculate("1.0.0")
+
+
+def test_expresion_syntax():
+    """
+    Проверяет синтаксес вырожения
+    :return: Данная функция ничего не возвращает
+    """
+
+    with pytest.raises(ExpresionException):
+        calculate("Hello world!")
+
+    with pytest.raises(ExpresionException):
+        calculate("")
+    with pytest.raises(ExpresionException):
+        calculate("(")
+    with pytest.raises(ExpresionException):
+        calculate(")")
+    with pytest.raises(ExpresionException):
+        calculate("()")
+    with pytest.raises(ExpresionException):
+        calculate("*")
+    with pytest.raises(ExpresionException):
+        calculate("+")
+
+    with pytest.raises(ExpresionException):
+        calculate("1*")
+    with pytest.raises(ExpresionException):
+        calculate("*1")
+    with pytest.raises(ExpresionException):
+        calculate("++")
+    with pytest.raises(ExpresionException):
+        calculate("++")
+
+    with pytest.raises(ExpresionException):
+        calculate("1)")
+    with pytest.raises(ExpresionException):
+        calculate(")1")
+    with pytest.raises(ExpresionException):
+        calculate("1(")
+    with pytest.raises(ExpresionException):
+        calculate("(1")
+
+    with pytest.raises(ExpresionException):
+        calculate("(1+1")
+    with pytest.raises(ExpresionException):
+        calculate("1+1)")
+    with pytest.raises(ExpresionException):
+        calculate("(-1)+((1+(2*3))+(-1)")
+    with pytest.raises(ExpresionException):
+        calculate("(-1)+(1+(2*3)))+(-1)")
